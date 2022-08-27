f1fd23100065a4be3ba69d77dc808b471579cad0
John <you@example.com>
2022-08-27 17:33:20 +0300

    fix(order):add validation query parameter with the help OrderGetInputDTO

--- src/Controller/Order/GetController.php
+++ src/Controller/Order/GetController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Order;
 
+use App\Helper\ValidatorInputDTO;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Order;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -11,6 +12,7 @@ use App\Helper\ValidatorPaginator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use App\DTO\OrderGetInputDTO;
 
 /**
  * @Route("/order", name="order_get", methods={"GET"})
@@ -20,20 +22,17 @@ class GetController extends AbstractController
 {
     public function __invoke(EntityManagerInterface $entityManager, Request $request, ValidatorInterface $validator): mixed
     {
-            /** @var ProductRepository $repoProduct */
-            $repoOrder = $entityManager->getRepository(Order::class);
-            $entityCount = $repoOrder->countEntity();
-            if ($entityCount < 1) {
-                throw new NotFoundHttpException ("Not found orders ");
-            }
+        $dto = new OrderGetInputDTO(
+            $page = $request->query->has('page') ? (int)$request->query->get('page') : 1,
+            $pageSize = $request->query->has('pageSize') ? (int)$request->query->get('pageSize') : 2,
+            $entityCount = (int)$entityManager->getRepository(Order::class)->countEntity(),
+        );
+        (new ValidatorInputDTO())->validateInput($validator, $dto);
 
-            $pageSize = $request->query->has('pageSize') ? $request->query->get('pageSize') : 2;
-            $page = $request->query->has('page') ? $request->query->get('page') : 1;
+        [$pageSize, $offset] = (new ValidatorPaginator)->offsetCalculateValidate($page, $pageSize, $entityCount, $validator);
 
-            [$pageSize, $offset] = (new ValidatorPaginator)->offsetCalculateValidate($page, $pageSize, $entityCount, $validator);
+        $ordersRepo = $entityManager->getRepository(Order::class)->findBy([], ["id" => "ASC"], $pageSize, $offset);
 
-            $ordersRepo = $entityManager->getRepository(Order::class)->findBy([], ["id" => "ASC"], $pageSize, $offset);
-
-            return (new Order())->arrOrderGetSerializer($ordersRepo);
+        return (new Order())->arrOrderGetSerializer($ordersRepo);
     }
 }
